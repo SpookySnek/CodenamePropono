6d03d78f3f797cdaf7782e23cc4000214cb0fc3d
SpookySnek <carleric.olsson@gmail.com>
2024-06-04 18:11:49 +0200

    Added docker & DbContext boilerplate

---
+++ CodenamePropono.Server/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using CodenamePropono.Server.Data;
+using CodenamePropono.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodenamePropono.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly ProponoDbContext _context;
+ 
+    public UsersController(ProponoDbContext context)
+    {
+        _context = context;
+    }
+ 
+    [HttpGet]
+    public async Task<IEnumerable<User>> Get()
+    {
+        return await _context.Users.ToListAsync();
+    }
+}
---
+++ CodenamePropono.Server/Data/ProponoDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using CodenamePropono.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CodenamePropono.Server.Data;
+
+public class ProponoDbContext : DbContext
+{
+    public ProponoDbContext(DbContextOptions<ProponoDbContext> options)
+        : base(options)
+    {
+        var dbCreater = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+        if(dbCreater != null)
+        {
+            // Create Database 
+            if(!dbCreater.CanConnect())
+            {
+                dbCreater.Create();
+            }
+ 
+            // Create Tables
+            if (!dbCreater.HasTables())
+            {
+                dbCreater.CreateTables();
+            }
+        }
+    }
+ 
+    public DbSet<User> Users { get; set; }
+    public DbSet<Collection> Collections { get; set; }
+    public DbSet<Photo> Photos { get; set; }
+ 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+            new User() { Id = 1337, Username = "MrCool", JoinDate = DateTime.Now, LastLogin = DateTime.Now }
+            );
+    }
+}
