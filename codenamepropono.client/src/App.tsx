90434501981b5408fc002c73af7d0d874f83a83b
SpookySnek <carleric.olsson@gmail.com>
2024-06-08 12:19:29 +0200

    Started client work & setup

--- codenamepropono.client/src/App.tsx
+++ codenamepropono.client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     }, []);
 
     const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+        ? <p><em>Propono is firing up...</em></p>
         : <table className="table table-striped" aria-labelledby="tabelLabel">
             <thead>
                 <tr>
@@ -40,7 +40,7 @@ function App() {
 
     return (
         <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
+            <h1 id="tabelLabel">Propono</h1>
             <p>This component demonstrates fetching data from the server.</p>
             {contents}
         </div>
